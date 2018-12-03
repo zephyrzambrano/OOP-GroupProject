c2e0ea477901ffb01e1b58a824ba813c6d6304dc
Khadija Rehman <kr521@scarletmail.rutgers.edu>
2018-12-03 14:32:49 -0500

    rates

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Enter company A years");
-		System.out.print("Enter company B years");
+		
+		
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Enter loan term (in years): ");
-	      double termInterestYears = scanner.nextInt();
+		
+	    
 	      InterestCalculator x=new InterestCalculator();
-			double a= scanner.nextDouble();
+	      System.out.print("Enter company A years"); 
+	      double a= scanner.nextDouble();
 			 a = x.calculateInterest(a);
+			 System.out.print("Enter company B years");
 			 double b =scanner.nextDouble();
 			 b = x.calculateInterest(b); 
 			System.out.print("company A interest is " + a);
 			System.out.print("company B interest is " + b);
-			
-		x.compareRates(a, b);
 		
-		System.out.print("The better interest rate is");
+			double betterrate = x.compareRates(a, b);
+		System.out.print("The better interest rate is" + betterrate);
 		
 	}
 
